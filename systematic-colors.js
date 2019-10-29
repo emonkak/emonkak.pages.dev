d4222dee4489eb01166516f4aa7e00cd5f9f2d27
Shota Nozaki <emonkak@gmail.com>
2019-10-29 21:07:01 +0900

    Update systematic-colors.js

--- systematic-colors.js
+++ systematic-colors.js
@@ -179,12 +179,13 @@ const VALUE_EASING = (t) => (--t) * t * t + 1;
 const RED = 3 / 360;
 const GREEN = 96 / 360;
 const BLUE = 204 / 360;
+const ORANGE = 28.2 / 360;
 
 const COLOR_DEFINITIONS = [
         ['CoolGray',  BLUE,                              SATURATION_EASING_FOR_BLACK, 0.10, 0.20, VALUE_EASING, 1.00, 0.00],
         ['WarmGray',  RED + (GREEN - RED) / 3 * 1,       SATURATION_EASING_FOR_BLACK, 0.10, 0.20, VALUE_EASING, 1.00, 0.00],
         ['Red',       RED,                               SATURATION_EASING, 1.10, 0.10, VALUE_EASING, 1.00, 0.00],
-        ['Orange',    RED + (GREEN - RED) / 3 * 1,       SATURATION_EASING, 1.10, 0.10, VALUE_EASING, 1.00, 0.00],
+        ['Orange' ,   ORANGE,                            SATURATION_EASING, 1.10, 0.10, VALUE_EASING, 1.00, 0.00],
         ['Lime'  ,    RED + (GREEN - RED) / 3 * 2,       SATURATION_EASING, 1.10, 0.10, VALUE_EASING, 1.00, 0.00],
         ['Green',     GREEN,                             SATURATION_EASING, 1.10, 0.10, VALUE_EASING, 1.00, 0.00],
         ['Teal',      GREEN + (BLUE - GREEN) / 3 * 1.5,  SATURATION_EASING, 1.10, 0.10, VALUE_EASING, 1.00, 0.00],
@@ -200,7 +201,7 @@ const GRAYSCALES =    [0.14, 0.24, 0.34, 0.44, 0.56, 0.68, 0.78, 0.86, 0.92, 0.9
 const DEBUG = false;
 
 const datasets = [];
-const variables = [];
+const variables = GRAYSCALES.slice().reverse().map((alpha, i) => `$white-${i + 1}: rgba(255, 255, 255, ${alpha});`);
 
 let container;
 
