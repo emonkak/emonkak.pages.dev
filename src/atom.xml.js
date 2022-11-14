f8d3bad08720ac19946deef5f924628d32bf9f87
Shota Nozaki <emonkak@gmail.com>
2022-11-14 14:25:47 +0900

    fix: atom feed format

--- src/atom.xml.js
+++ src/atom.xml.js
@@ -9,13 +9,12 @@ export default function render({ context }) {
     return u('root', [
         u('instruction', { name: 'xml' }, 'version="1.0" encoding="utf-8"'),
         x('feed', { xmlns: 'http://www.w3.org/2005/Atom' }, [
-            x('title', ['My Blog']),
-            x('links', [
-                x('link', { type: 'text/html', href: BASE_URL }),
-            ]),
+            x('title', { type: 'text' }, ['My Blog']),
             x('id', [BASE_URL]),
+            x('link', { type: 'text/html', rel: 'alternate', href: BASE_URL }),
+            x('link', { type: 'application/atom+xml', rel: 'self', href: BASE_URL + 'atom.xml' }),
             x('updated', [context.lastUpdated().toISOString()]),
-            x('entries', context.sortedArticles().slice(0, MAX_FEED_ENTRIES).map(renderEntry)),
+            context.sortedArticles().slice(0, MAX_FEED_ENTRIES).map(renderEntry),
         ]),
     ]);
 }
@@ -24,15 +23,13 @@ function renderEntry(resource) {
     const url = BASE_URL + resource.mountPath;
     return x('entry', [
         x('title', [resource.matter?.title ?? resource.mountPath]),
-        x('links', [
-            x('link', { type: 'text/html', href: url }),
-        ]),
         x('id', [url]),
+        x('link', { type: 'text/html', ref: 'alternate', href: url }),
         x('updated', [resource.stats.mtime.toISOString()]),
         resource.matter.date && x('published', [resource.matter.date]),
-        resource.matter.tags && x('categories', resource.matter.tags.map(renderCategory)),
-        resource.matter.summary && x('summary', [resource.matter.summary]),
-        x('content', { type: 'text/html' }, [
+        resource.matter.tags && resource.matter.tags.map(renderCategory),
+        resource.matter.summary && x('summary', { type: 'text' }, [resource.matter.summary]),
+        x('content', { type: 'html' }, [
             u('cdata', toHtml(resource.content, { allowDangerousHtml: true })),
         ])
     ])
