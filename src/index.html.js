3b16d44be1eff514150113dd18607b50d03d0d7b
Shota Nozaki <emonkak@gmail.com>
2023-01-30 13:33:40 +0900

    feat: add metadata for the top page

--- src/index.html.js
+++ src/index.html.js
@@ -3,6 +3,7 @@ import { toText } from 'hast-util-to-text';
 
 import renderLayout from '../lib/templates/layout.js';
 import renderArticleCardList from '../lib/templates/articleCardList.js';
+import { BASE_URL, DESCRIPTION, SITE_NAME } from '../lib/constants.js';
 
 const NUM_ARTICLES = 10;
 
@@ -10,6 +11,7 @@ export default function render(props) {
     const { resource, site } = props;
     return renderLayout({
         content: renderContent(props),
+        additionalMetadata: renderAdditionalMetadata(resource),
         site,
         resource,
     });
@@ -30,4 +32,14 @@ function renderContent(props) {
             ])
         ]),
     ]);
+}
+
+function renderAdditionalMetadata() {
+    return [
+        h('meta', { property: 'description', content: DESCRIPTION }),
+        h('meta', { property: 'og:title', content: SITE_NAME }),
+        h('meta', { property: 'og:description', content: DESCRIPTION }),
+        h('meta', { property: 'og:url', content: BASE_URL + '/' }),
+        h('meta', { property: 'og:type', content: 'website' }),
+    ];
 }
